8182a870a7b2dcbdb853fc08e13557c024f81520
Cal <khalfanijabari@gmail.com>
2022-08-30 13:46:02 -0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CreateClientComponent from "./components/CreateClientComponent";
 import MainPageComponent from "./components/MainPageComponent";
 import ContactComponent from "./components/ContactComponent";
 function App() {
+  // The key to GitHub pages deployeent is to...
+  // 1.) add "basename=..." in the router.
+  // Although, routing to other pages is still a problem on GH pages
+  console.log(process.env.PUBLIC_URL);
   return (
     <div style={{ backgroundColor: "#F9F1E9", width: "100%" }}>
       <div>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route path="/" exact={true} component={MainPageComponent}></Route>
             <Route path="/add-client" component={CreateClientComponent}></Route>
