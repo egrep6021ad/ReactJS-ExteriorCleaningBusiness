ce7cf6329748f7626fcbf502bd72648e6f9764a3
Cal <khalfanijabari@gmail.com>
2022-08-30 13:13:20 -0400

    gh-pages

--- src/components/MasonryImageComponent.jsx
+++ src/components/MasonryImageComponent.jsx
@@ -1,19 +1,21 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import window from './window.jpg'
-import niceHouse from './niceHouse.jpg'
-import niceGrey from './niceGrey.jpg'
-import window2 from './window2.jpg'
-import brickHouse from './brickHouse.jpg'
-import trashCans from './trashCans.jpg'
-import trucks from './trucks.jpg'
-import bHouse from './bHouse.jpg'
-import mossHouse from './mossHouse.jpg'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import window from "./window.jpg";
+import niceHouse from "./niceHouse.jpg";
+import niceGrey from "./niceGrey.jpg";
+import window2 from "./window2.jpg";
+import brickHouse from "./brickHouse.jpg";
+import trashCans from "./trashCans.jpg";
+import trucks from "./trucks.jpg";
+import bHouse from "./bHouse.jpg";
+import mossHouse from "./mossHouse.jpg";
 function MasonryImageList() {
   return (
-    <Box sx={{ paddingLeft:'5%',width: '95%', height: 275, overflow: 'scroll' }}>
+    <Box
+      sx={{ paddingLeft: "5%", width: "95%", height: 575, overflow: "scroll" }}
+    >
       <ImageList variant="masonry" cols={2} gap={8}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
@@ -33,51 +35,47 @@ function MasonryImageList() {
 const itemData = [
   {
     img: window,
-    title: 'Clean Residential Exterior Deck in Atlanta, North Georgia',
-
+    title: "Clean Residential Exterior Deck in Atlanta, North Georgia",
   },
   {
     img: niceHouse,
-    title: 'Clean Residential Exterior Roof and Driveway in Atlanta, Georgia',
-
+    title: "Clean Residential Exterior Roof and Driveway in Atlanta, Georgia",
   },
   {
     img: niceGrey,
-    title: 'Clean Residential Exterior Driveway and Roof in Atlanta, North Georgia',
- 
+    title:
+      "Clean Residential Exterior Driveway and Roof in Atlanta, North Georgia",
   },
   {
     img: window2,
-    title: 'premier commercial cleaning of industrial exterior and roof in Atlanta, Georgia',
-
+    title:
+      "premier commercial cleaning of industrial exterior and roof in Atlanta, Georgia",
   },
   {
     img: brickHouse,
-    title: 'Underground Atlanta Clean Commercial Exterior Services, best premier cleaning in North Georgia',
+    title:
+      "Underground Atlanta Clean Commercial Exterior Services, best premier cleaning in North Georgia",
   },
 
   {
     img: trashCans,
-    title: 'Clean residential house, exterior roof and driveway in Atlanta, north deorgia',
-
+    title:
+      "Clean residential house, exterior roof and driveway in Atlanta, north deorgia",
   },
   {
     img: trucks,
-    title: 'Clean residential house, exterior roof and driveway in Atlanta, north deorgia',
-
+    title:
+      "Clean residential house, exterior roof and driveway in Atlanta, north deorgia",
   },
   {
     img: bHouse,
-    title: 'Clean residential house, exterior roof and driveway in Atlanta, north deorgia',
-
+    title:
+      "Clean residential house, exterior roof and driveway in Atlanta, north deorgia",
   },
   {
     img: mossHouse,
-    title: 'Clean residential house, exterior roof and driveway in Atlanta, north deorgia',
-
-  }
-
-
- 
+    title:
+      "Clean residential house, exterior roof and driveway in Atlanta, north deorgia",
+  },
 ];
-export default MasonryImageList
+export default MasonryImageList;
