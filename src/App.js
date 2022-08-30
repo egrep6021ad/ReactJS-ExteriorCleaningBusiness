5df1cfe225a0edba353407b1e1357d54e18607d6
Cal <khalfanijabari@gmail.com>
2022-08-30 13:24:58 -0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import CreateClientComponent from './components/CreateClientComponent';
-import MainPageComponent from './components/MainPageComponent';
-import ContactComponent from './components/ContactComponent';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import CreateClientComponent from "./components/CreateClientComponent";
+import MainPageComponent from "./components/MainPageComponent";
+import ContactComponent from "./components/ContactComponent";
 function App() {
   return (
-
-
-    <div style={{backgroundColor:'#F9F1E9', width:'100%'}}> 
-            <div>
-              <Router> 
-        
-                <Switch>  
-                  <Route path ="/" exact={true} component = {MainPageComponent}></Route>
-                  <Route path ="/add-client" component = {CreateClientComponent}></Route>
-                  <Route path ="/contact" component = {ContactComponent}></Route>
-
-                </Switch>
-              </Router>
-            </div>
+    <div style={{ backgroundColor: "#F9F1E9", width: "100%" }}>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact={true} component={MainPageComponent}></Route>
+            <Route path="/add-client" component={CreateClientComponent}></Route>
+            <Route path="/contact" component={ContactComponent}></Route>
+          </Switch>
+        </Router>
       </div>
+    </div>
   );
 }
 export default App;
